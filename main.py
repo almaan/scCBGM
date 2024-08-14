d0a97935ab0efceef5c1b508370a12358da2396e
ismaia11 <ismaia11@gene.com>
2024-08-14 16:48:37 -0400

    update testing

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from lightning.pytorch.loggers import WandbLogger
 
 import pytorch_lightning as pl
 import anndata as ad
+import xarray as xr
 
 
 if __name__ == "__main__":
@@ -19,12 +20,11 @@ if __name__ == "__main__":
     checkpoint_dir = "/data/bucket/ismaia11/conceptlab/models/"
 
     # n_obs = 50000
-    # n_vars = 34455
-    # # n_vars = 300
-    # n_tissues = 3
-    # n_celltypes = 10
-    # n_batches = 2
-    # n_concepts = 8
+    n_vars = 34455
+    n_tissues = 3
+    n_celltypes = 10
+    n_batches = 2
+    n_concepts = 8
 
  
     # dataset = clab.datagen.omics.OmicsDataGenerator.generate(n_obs = n_obs,
@@ -35,29 +35,41 @@ if __name__ == "__main__":
     #                                                          n_concepts = n_concepts,
     #                                                         )
 
-    # data_module = clab.datagen.dataloader.GeneExpressionDataModule(dataset, batch_size=512)
 
-    data_pth = "./data/5a611776-aae0-41b9-9f2b-aaf5f83771a3.h5ad"
-
-    adata = ad.read_h5ad(data_pth) 
-    subset_cells = adata[0:50000, :]
-
-    # subset_cells.write("./data/subset_cells.h5ad")
+    # dataset.to_netcdf('./data/complete_set_dataset.nc')
 
 
+    
+    # data_pth = "./data/5a611776-aae0-41b9-9f2b-aaf5f83771a3.h5ad"
 
-    # print(adata.shape)
-    # print(subset_cells.shape)
+    # adata = ad.read_h5ad(data_pth) 
+    # subset_cells = adata[0:50000, :]
 
-    # print(stop)
-    # data_pth = "./data/subset_cells.h5ad"
-    # dataset = ad.read_h5ad(data_pth) 
-    n_samples,n_vars = subset_cells.shape
-    # print(n_vars)
-    # print(stp)
-    data_module = clab.datagen.dataloader.GeneExpressionDataModule(subset_cells, batch_size=512) 
+    # # subset_cells.write("./data/subset_cells.h5ad")
 
 
+
+    # # print(adata.shape)
+    # # print(subset_cells.shape)
+
+    # # print(stop)
+    # # data_pth = "./data/subset_cells.h5ad"
+    # # dataset = ad.read_h5ad(data_pth) 
+    # n_samples,n_vars = subset_cells.shape
+
+    # data_module = clab.datagen.dataloader.GeneExpressionDataModule(subset_cells, batch_size=512) 
+
+
+
+    print("Reading")
+    dataset = xr.open_dataset('./data/complete_set_dataset.nc')
+
+    print("Creating a data_module")
+    data_module = clab.datagen.dataloader.GeneExpressionDataModule(dataset, batch_size=512)
+
+
+
+    print("Training")
     vae = clab.models.VAE(input_dim=n_vars,
                 hidden_dim=1024, #these numbers are quite arbitrary
                 latent_dim=512,
@@ -66,7 +78,7 @@ if __name__ == "__main__":
 
 
     wandb_logger = WandbLogger(project='conceptlab_sync', # group runs in "MNIST" project
-                                name="vae_real",
+                                name="vae",
                                 log_model='all') 
 
 
@@ -76,7 +88,7 @@ if __name__ == "__main__":
 
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
         dirpath=checkpoint_dir,
-        filename='vae_real',
+        filename='vae',
         save_top_k=1,
         verbose=True,
         monitor='val_Total_loss',
@@ -89,6 +101,8 @@ if __name__ == "__main__":
 
     # data_module = clab.datagen.dataloader.GeneExpressionDataModule(dataset,add_concepts=True, batch_size=512) 
 
+
+    # print("Training")
     # vae = clab.models.CB_VAE(input_dim=n_vars,
     #             hidden_dim=1024, #these numbers are quite arbitrary
     #             latent_dim=512,
