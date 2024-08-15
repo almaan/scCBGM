0b8f7f1024730b9269aa71f1706d66aa6d72eec4
Andersson <andera29@sc1nc004is02.eth.rsiec.sc1.science.roche.com>
2024-08-15 09:04:42 -0700

    change n_vars and layer sizes

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ if __name__ == "__main__":
     checkpoint_dir = "/data/bucket/ismaia11/conceptlab/models/"
 
     # n_obs = 50000
-    # n_vars = 34455
-    # # n_vars = 300
+    # n_vars = 5000
     # n_tissues = 3
     # n_celltypes = 10
     # n_batches = 2
@@ -40,7 +39,7 @@ if __name__ == "__main__":
     data_pth = "./data/5a611776-aae0-41b9-9f2b-aaf5f83771a3.h5ad"
 
     adata = ad.read_h5ad(data_pth) 
-    subset_cells = adata[0:50000, :]
+    subset_cells = adata[0:50000, 0:5000]
 
     # subset_cells.write("./data/subset_cells.h5ad")
 
@@ -59,8 +58,8 @@ if __name__ == "__main__":
 
 
     vae = clab.models.VAE(input_dim=n_vars,
-                hidden_dim=1024, #these numbers are quite arbitrary
-                latent_dim=512,
+                hidden_dim=128, #these numbers are quite arbitrary
+                latent_dim=64,
                 beta = 0.01,
                 learning_rate=1e-3)
 
@@ -90,8 +89,8 @@ if __name__ == "__main__":
     # data_module = clab.datagen.dataloader.GeneExpressionDataModule(dataset,add_concepts=True, batch_size=512) 
 
     # vae = clab.models.CB_VAE(input_dim=n_vars,
-    #             hidden_dim=1024, #these numbers are quite arbitrary
-    #             latent_dim=512,
+    #             hidden_dim=128, #these numbers are quite arbitrary
+    #             latent_dim=64,
     #             n_concepts=n_concepts,
     #             beta = 1e-3,
     #             learning_rate=1e-4)
