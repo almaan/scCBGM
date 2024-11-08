4df90a2441b9d8ff1511d3b7b4b58dca5802666b
andera29 <andera29@gene.com>
2024-11-08 09:42:48 -0800

    import fix

--- conceptlab/models/__init__.py
+++ conceptlab/models/__init__.py
@@ -1,6 +1,6 @@
 from .vae import VAE
 from .nb_cb_vae import NB_CB_VAE
-from .cb_vae import CB_VAE, SCALED_CB_VAE, DIFF_CB_VAE
+from .cb_vae import CB_VAE, SCALED_CB_VAE
 from .cem_vae import CEM_VAE
 
 from .dev import DEV
